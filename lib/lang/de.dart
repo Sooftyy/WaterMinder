2837d2c88fcee515290f21fa12cacd3bc633b531
Sooftyy <Steven.heigus@gmail.com>
2024-09-24 13:58:33 +0200

    Language and Timezone/Time Formate Change

---
+++ lib/lang/de.dart
@@ -0,0 +1,14 @@
+const Map<String, String> de = {
+  'total_ml': 'Gesamt ML',
+  'delete': 'Löschen',
+  'confirm_delete_drink': 'Möchten Sie dieses Getränk wirklich löschen?',
+  'cancel': 'Abbrechen',
+  'add_drink': 'Getränk hinzufügen',
+  'drink_name': 'Getränkename',
+  'ml': 'ML',
+  'add': 'Hinzufügen',
+  'language': 'Sprache',
+  'time_format': 'Zeitformat',
+  'select_language': 'Sprache wählen',
+  'select_time_format': 'Zeitformat wählen',
+};
